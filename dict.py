648b85ec145b2be97f7aee2a6916488bfe941d89
JakubGniazdowski <98549776+JakubGniazdowski@users.noreply.github.com>
2022-02-17 10:32:40 +0100

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,3 +1,27 @@
+import difflib
 import json
-data = json.load(open('data.json','r'))
-print(data)
+data = json.load(open('data.json'))
+
+def translation(word):
+    word=word.lower()
+    if word in data:
+        return data[word]
+    elif len(difflib.get_close_matches(word ,data.keys()))>0:
+        yn=input("did you mean %s instead. Enter Y if yes N if no: " % difflib.get_close_matches(word ,data.keys())[0])
+        if yn=="Y":
+            return data[difflib.get_close_matches(word ,data.keys())[0]]
+        if yn=="N":
+            return "there is no word like this in our dictionary, sorry"
+        else:
+            return "we didnt understand your entry"
+    else:
+        return "there is no word like this in our dictionary, sorry"
+        
+
+word=input("put here word that you search for: ")
+output=translation(word)
+if type(output)== list:
+    for item in output:
+        print(item)
+else:
+    print(output)
